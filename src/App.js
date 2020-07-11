7a31f66ed0c41cc94c596ef57ecfd26e5d8dafc6
MuhammadAmmarSiddiqui <ammar-sidd@hotmail.com>
2020-07-10 23:49:19 -0700

    Completed Covid19 tracker with only stats

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NavBar from './Components/Navbar'
+import InfoPanel from './Components/infopanel'
+import BottomNav from './Components/BottomNav'
 
 function App() {
+
+  const screenConfig = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <InfoPanel currentScreen={screenConfig[0]}/>
+      <BottomNav screenConfig={screenConfig} />
     </div>
   );
 }
